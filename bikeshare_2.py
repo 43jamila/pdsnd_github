dcae297b2dec70cd781bdd81aed81ff60262aa2a
Jamilah Asiri <46827198+43jamila@users.noreply.github.com>
2022-12-15 06:44:08 +0300

    add some iformation of programmer as comments

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -1,6 +1,7 @@
 import time
 import pandas as pd
 
+# name of programmer jamilah asiri
 CITY_DATA = {'chicago': 'chicago.csv',
              'new york city': 'new_york_city.csv',
              'washington': 'washington.csv'}
